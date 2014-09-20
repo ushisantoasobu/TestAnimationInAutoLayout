8d185acc0f1c1546bc620b1c865040fb2a1f9f1f
ushisantoasobu <babblemann.shunsee@gmail.com>
2014-09-20 22:10:29 +0900

    first commit

--- TestAnimationInAutoLayout/ViewController.m
+++ TestAnimationInAutoLayout/ViewController.m
@@ -10,6 +10,10 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *yConst;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *xConst;
+
+
 @end
 
 @implementation ViewController
@@ -23,5 +27,37 @@
     [super didReceiveMemoryWarning];
     // Dispose of any resources that can be recreated.
 }
+
+- (IBAction)btnTapped:(id)sender {
+    
+    //http://stackoverflow.com/questions/19899323/autolayout-constraints-and-animation
+    //より
+    
+    [self.view removeConstraints:@[self.yConst,self.xConst]];
+    CGFloat xPos = rand() % (int)self.view.frame.size.width;
+    CGFloat yPos = rand() % (int)self.view.frame.size.height;
+    self.yConst = [NSLayoutConstraint constraintWithItem:self.view
+                                                  attribute:NSLayoutAttributeBottom
+                                                  relatedBy:0
+                                                     toItem:sender
+                                                  attribute:NSLayoutAttributeBottom
+                                                 multiplier:1
+                                                   constant:yPos];
+    [self.view addConstraint:self.yConst];
+    self.xConst = [NSLayoutConstraint constraintWithItem:self.view
+                                                          attribute:NSLayoutAttributeRight
+                                                          relatedBy:0
+                                                             toItem:sender
+                                                          attribute:NSLayoutAttributeRight
+                                                         multiplier:1
+                                                           constant:xPos];
+    [self.view addConstraint:self.xConst];
+    [UIView animateWithDuration:1 delay:0.0 options:0 animations:^{
+        [self.view layoutIfNeeded];
+    } completion:^(BOOL finished) {
+        //
+    }];
+}
+
 
 @end
