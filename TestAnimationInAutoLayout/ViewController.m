60ea312a902ac512ad68ca039a74bb442705d6dd
ushisantoasobu <babblemann.shunsee@gmail.com>
2014-09-21 01:26:19 +0900

    like Tmblr animation

--- TestAnimationInAutoLayout/ViewController.m
+++ TestAnimationInAutoLayout/ViewController.m
@@ -10,17 +10,35 @@
 
 @interface ViewController ()
 
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *yConst;
-@property (weak, nonatomic) IBOutlet NSLayoutConstraint *xConst;
+@property (weak, nonatomic) IBOutlet UIButton *btn1;
+@property (weak, nonatomic) IBOutlet UIButton *btn2;
+@property (weak, nonatomic) IBOutlet UIButton *btn3;
+@property (weak, nonatomic) IBOutlet UIButton *btn4;
 
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *constraint1_y;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *constraint2_y;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *constraint3_y;
+@property (weak, nonatomic) IBOutlet NSLayoutConstraint *constraint4_y;
+
+@property (strong, nonatomic) NSLayoutConstraint *tempConstraint1_y;
+@property (strong, nonatomic) NSLayoutConstraint *tempConstraint2_y;
+@property (strong, nonatomic) NSLayoutConstraint *tempConstraint3_y;
+@property (strong, nonatomic) NSLayoutConstraint *tempConstraint4_y;
+
+@property (weak, nonatomic) IBOutlet UIButton *btnTest;
 
 @end
+
+//http://stackoverflow.com/questions/19899323/autolayout-constraints-and-animation
+//より
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+    
+    [self setViewBelowPosition:NO];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -28,36 +46,120 @@
     // Dispose of any resources that can be recreated.
 }
 
-- (IBAction)btnTapped:(id)sender {
+
+
+- (void)setViewBelowPosition:(BOOL)animate
+{
+    CGFloat duration = animate ? 0.4 : 0.0;
     
-    //http://stackoverflow.com/questions/19899323/autolayout-constraints-and-animation
-    //より
-    
-    [self.view removeConstraints:@[self.yConst,self.xConst]];
-    CGFloat xPos = rand() % (int)self.view.frame.size.width;
-    CGFloat yPos = rand() % (int)self.view.frame.size.height;
-    self.yConst = [NSLayoutConstraint constraintWithItem:self.view
+    _tempConstraint1_y = _constraint1_y;
+    [self.view removeConstraints:@[_constraint1_y]];
+    _constraint1_y = [NSLayoutConstraint constraintWithItem:self.view
                                                   attribute:NSLayoutAttributeBottom
                                                   relatedBy:0
-                                                     toItem:sender
+                                                     toItem:_btn1
                                                   attribute:NSLayoutAttributeBottom
                                                  multiplier:1
-                                                   constant:yPos];
-    [self.view addConstraint:self.yConst];
-    self.xConst = [NSLayoutConstraint constraintWithItem:self.view
-                                                          attribute:NSLayoutAttributeRight
-                                                          relatedBy:0
-                                                             toItem:sender
-                                                          attribute:NSLayoutAttributeRight
-                                                         multiplier:1
-                                                           constant:xPos];
-    [self.view addConstraint:self.xConst];
-    [UIView animateWithDuration:1 delay:0.0 options:0 animations:^{
+                                                   constant:-1000];
+    [self.view addConstraint:_constraint1_y];
+    [UIView animateWithDuration:duration delay:0.0 options:0 animations:^{
+        [self.view layoutIfNeeded];
+    } completion:^(BOOL finished) {
+        //
+    }];
+    
+    _tempConstraint2_y = _constraint2_y;
+    [self.view removeConstraints:@[_constraint2_y]];
+    _constraint2_y = [NSLayoutConstraint constraintWithItem:self.view
+                                                  attribute:NSLayoutAttributeBottom
+                                                  relatedBy:0
+                                                     toItem:_btn2
+                                                  attribute:NSLayoutAttributeBottom
+                                                 multiplier:1
+                                                   constant:-1000];
+    [self.view addConstraint:_constraint2_y];
+    [UIView animateWithDuration:duration delay:0.0 options:0 animations:^{
+        [self.view layoutIfNeeded];
+    } completion:^(BOOL finished) {
+        //
+    }];
+    
+    _tempConstraint3_y = _constraint3_y;
+    [self.view removeConstraints:@[_constraint3_y]];
+    _constraint3_y = [NSLayoutConstraint constraintWithItem:self.view
+                                                  attribute:NSLayoutAttributeBottom
+                                                  relatedBy:0
+                                                     toItem:_btn3
+                                                  attribute:NSLayoutAttributeBottom
+                                                 multiplier:1
+                                                   constant:-1000];
+    [self.view addConstraint:_constraint3_y];
+    [UIView animateWithDuration:duration delay:0.0 options:0 animations:^{
+        [self.view layoutIfNeeded];
+    } completion:^(BOOL finished) {
+        //
+    }];
+    
+    _tempConstraint4_y = _constraint4_y;
+    [self.view removeConstraints:@[_constraint4_y]];
+    _constraint4_y = [NSLayoutConstraint constraintWithItem:self.view
+                                                  attribute:NSLayoutAttributeBottom
+                                                  relatedBy:0
+                                                     toItem:_btn4
+                                                  attribute:NSLayoutAttributeBottom
+                                                 multiplier:1
+                                                   constant:-1000];
+    [self.view addConstraint:_constraint4_y];
+    [UIView animateWithDuration:duration delay:0.0 options:0 animations:^{
         [self.view layoutIfNeeded];
     } completion:^(BOOL finished) {
         //
     }];
 }
+
+- (IBAction)btnTapped:(id)sender {
+    
+    _btnTest.selected = !_btnTest.selected;
+    
+    if (!_btnTest.isSelected) {
+        [self setViewBelowPosition:YES];
+        return;
+    }
+    
+    [self.view removeConstraints:@[_constraint1_y]];
+    [self.view addConstraint:_tempConstraint1_y];
+    [UIView animateWithDuration:0.4 delay:0.0 options:0 animations:^{
+        [self.view layoutIfNeeded];
+    } completion:^(BOOL finished) {
+        _constraint1_y = _tempConstraint1_y;
+    }];
+    
+    [self.view removeConstraints:@[_constraint2_y]];
+    [self.view addConstraint:_tempConstraint2_y];
+    [UIView animateWithDuration:0.4 delay:0.2 options:0 animations:^{
+        [self.view layoutIfNeeded];
+    } completion:^(BOOL finished) {
+        _constraint2_y = _tempConstraint2_y;
+    }];
+    
+    [self.view removeConstraints:@[_constraint3_y]];
+    [self.view addConstraint:_tempConstraint3_y];
+    [UIView animateWithDuration:0.4 delay:0.4 options:0 animations:^{
+        [self.view layoutIfNeeded];
+    } completion:^(BOOL finished) {
+        _constraint3_y = _tempConstraint3_y;
+    }];
+    
+    [self.view removeConstraints:@[_constraint4_y]];
+    [self.view addConstraint:_tempConstraint4_y];
+    [UIView animateWithDuration:0.4 delay:0.6 options:0 animations:^{
+        [self.view layoutIfNeeded];
+    } completion:^(BOOL finished) {
+        _constraint4_y = _tempConstraint4_y;
+    }];
+}
+
+
 
 
 @end
